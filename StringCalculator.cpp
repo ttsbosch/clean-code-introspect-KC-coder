2d23d2fcdad969caa32539b4d4f945dd000ed4c7
KC-coder <80200555+KC-coder@users.noreply.github.com>
2024-06-27 16:05:48 +0530

    Update_18

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+
+int StringCalculator::getValueofNumber(int postion , const string &str )
+{
+    int value = 0;
+    std::cout << "(postion - 1)= " << (postion - 1) <<endl;
+    if((postion - 1) == 0)
+    {
+        std::cout << "str[postion - 1]= " << str[postion - 1] <<endl;
+      if(str[postion - 1] == '-')
+      {
+          std::cout << "str[postion - 1]= " << str[postion - 1] <<endl;
+          throw  std::runtime_error("Negative number in string");
+      }
+    }
+    else
+    {
+        value = (str[postion] - 48);
+    }
+    return value;
+}
+
 int StringCalculator::add(string str)
 {
   int finalValue = 0;
