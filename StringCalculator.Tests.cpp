c10d7641c9d3d48344a419256c461622a6b1f8f9
KC-coder <80200555+KC-coder@users.noreply.github.com>
2024-06-27 16:15:12 +0530

    Update_22_FinalGame

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -44,7 +44,7 @@ TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
 
     ASSERT_EQ(result, expectedresult);
 }
-/*
+
 TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
     int expectedresult = 1;
     std::string input = "1,1001";
@@ -61,4 +61,4 @@ TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
     int result = objUnderTest.add(input);
 
     ASSERT_EQ(result, expectedresult);
-}*/
+}
